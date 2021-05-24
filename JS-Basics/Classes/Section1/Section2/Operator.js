32380f988136563b2aeced104728f48c1a9bae9b
FacuFrancia <facundofrancia3@hotmail.com>
2021-05-24 09:42:51 -0300

    AssigmentOperators

---
+++ JS-Basics/Classes/Section1/Section2/Operator.js
@@ -0,0 +1,17 @@
+let x = 10;
+let y = 3;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(x ** y);
+
+// Increment (++)
+console.log(x++); //Shows x and the increments 1 
+console.log(++x); //Increments and shows
+
+// Decrement (--)
+console.log(x--); //Shows x and the Decrements 1 
+console.log(--x)  //Decrements and the shows x
--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,16 +1,2 @@
-//Functions - Set of Statments 
-
-//Performing a Task
-function greet(name, lastName) {
-    console.log('Hello ' + name + ' ' + lastName);
-}
-
-greet('John', "Smith"); //Calling greet(argument);
-
-// Calculating a value
-function square(number) {
-    return number * number;
-}
-
-console.log(square(2));
+let x = 1;
 
