8d4e7d0f12924ae3a8d5a793b91efdf60b9c6fb6
FacuFrancia <facundofrancia3@hotmail.com>
2021-05-26 11:13:11 -0300

    Excercise10.

---
+++ JS-Basics/Classes/Section3/Excercise10.js
@@ -0,0 +1,12 @@
+// Rows //
+function showStars(rows) {
+    for (let row = 1; row <= rows; row++) {
+        let pattern = '';
+        for (let i = 0; i < row; i++)
+            pattern += '*';
+        console.log(pattern);
+    }
+
+}
+
+const rows = showStars(5);
