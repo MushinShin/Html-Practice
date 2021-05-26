903e1e838c041950e8b2857b3498bb33e788ac29
FacuFrancia <facundofrancia3@hotmail.com>
2021-05-26 11:48:35 -0300

    FactoryFunction.

---
+++ JS-Basics/Classes/Section4/FactoryFunction.js
@@ -0,0 +1,23 @@
+
+// Factory Function
+//Define logic in 1 place 
+//Call function with diferent values but there
+//  is a single place to modify
+
+
+function createCircle(radius) {
+    return {
+        radius,
+        draw() {
+            console.log('draw');
+
+        }
+    };
+}
+
+
+const circle1 = createCircle(1);
+console.log(circle1);
+
+const circle2 = createCircle(2);
+console.log(circle2);
