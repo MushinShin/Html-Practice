b196b1ab1980df7196b24aebe2d878f14cd67766
FacuFrancia <facundofrancia3@hotmail.com>
2021-05-27 11:12:23 -0300

    RemovingElements.

---
+++ JS-Basics/Classes/Section5/RemovingElements.js
@@ -0,0 +1,17 @@
+//  Removing Elements
+
+const numbers = [1, 2, 3, 4];
+
+// End --------
+const last = numbers.pop();
+console.log(numbers);
+console.log(last);
+
+
+// Beginning ------
+const first = numbers.shift();
+console.log(first);
+
+// Middle ---------
+numbers.splice(2, 2);
+console.log(numbers);
