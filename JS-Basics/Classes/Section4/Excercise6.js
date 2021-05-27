d1ea9d70b34eaca2052ac024066ea987dccdc2d7
FacuFrancia <facundofrancia3@hotmail.com>
2021-05-27 10:47:54 -0300

    Excercise6.

---
+++ JS-Basics/Classes/Section4/Excercise6.js
@@ -0,0 +1,6 @@
+// Array of Objects each is price range object (4)
+let priceRanges = [
+    { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 }
+    { label: '$$', tooltip: 'Moderate', minPerPerson: 11, maxPerPerson: 20 }
+    { label: '$$$', tooltip: 'Inexpensive', minPerPerson: 21, maxPerPerson: 50 }
+]
