36b7feb7644edae5b19406d6cb76d47f6be04fb2
FacuFrancia <facundofrancia3@hotmail.com>
2021-05-27 11:40:30 -0300

    joiningarrays

---
+++ JS-Basics/Classes/Section5/JoiningArrays.js
@@ -0,0 +1,12 @@
+//  Joining Arrays.
+const numbers = [1, 2, 3];
+const joined = numbers.join(',');
+console.log(joined);
+
+
+const message = 'This is my pen';
+const parts = message.split(' ');
+console.log(parts);
+
+const combined = parts.join('-');
+console.log(combined);
