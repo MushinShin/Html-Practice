9ce6a14cd67799ccb0dfe343364a244c2182911c
FacuFrancia <facundofrancia3@hotmail.com>
2021-05-29 12:13:26 -0300

    Excercise5.

---
+++ JS-Basics/Classes/Section5/Excercise5.js
@@ -0,0 +1,19 @@
+const number = [1, 2, 3, 4, 1];
+
+const count = countOccurrences(number, -1);
+
+console.log(count);
+
+function countOccurrences(array, searchElement) {
+    // let count = 0;
+    // for (let element of array)
+    //     if (element === searchElement)
+    //         count++;
+    // return count;
+
+    return array.reduce((accumulator, current) => {
+        const occurrence = (current === searchElement) ? 1 : 0;
+        console.log(accumulator, current, searchElement);
+        return accumulator + occurrence;
+    }, 0);
+}
