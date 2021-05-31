faa8f8713ae55415cf5ccc8ed8f6b1f5a9b7de15
FacuFrancia <facundofrancia3@hotmail.com>
2021-05-31 10:01:43 -0300

    FunctionDec-Exp

---
+++ JS-Basics/Classes/Section6/FunctionDec-Exp.js
@@ -0,0 +1,23 @@
+walk();
+
+// Function Declaration
+function walk() {
+    console.log('walk')
+}
+
+// Anonymous Function Expression
+run();
+
+
+let run = function () {
+    console.log('run');
+};
+
+
+
+// Named Function Expression
+// let run1 = function walk() {
+//     console.log('run');
+// };
+
+// Hoisting is moving automatically functions to the top
